df2b69fe15a2cd4fe9ebc379a06cb4b0761d4047
Gabin Marignier <gabin.contact@gmail.com>
2018-05-04 11:10:47 +0200

    Add flamegraph mock server

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -49,7 +48,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func flamegraphHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "flamegraph")
+	http.ServeFile(w, r, "./flamegraph.svg")
 }
 
 func main() {
